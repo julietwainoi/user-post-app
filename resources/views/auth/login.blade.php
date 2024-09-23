d64effa5065c74aecb4e981a2b5a2e55b2fb79dd
julietwainoi <juliewainoi@gmail.com>
2024-09-23 12:12:20 +0300

    user-post-app

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,95 @@
+@extends('auth.Layouts.app')
+@section('content')
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="" class="h1">Sign In</a>
+        </div>
+        <div class="card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div class="input-group mb-3">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                        placeholder="Email">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+        <!-- Password -->
+        <div class="input-group mb-3">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="current-password" placeholder="Password">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+        <!-- Remember Me -->
+        <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center mt-2 mb-3">
+                <a href="#" class="">
+                    <i class=""></i>
+                </a>
+                <a href="#" class="">
+                    <i class=""></i>
+                </a>
+            </div>
+            <!-- /.social-auth-links -->
+            <p class="mb-1">
+                <a href="">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+                <a href="" class="text-center"></a>
+                
+
+        
+
+            </p>
+            <p class="mb-0">
+                <!-- HTML button to navigate to the registration route -->
+            <a href="" >create account?</a>
+
+                
+        </p>
+            
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+
+@endsection
