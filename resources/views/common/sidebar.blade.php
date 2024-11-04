d483d706071bbfe4f4dd4bb348b40cf22252c396
julietwainoi <juliewainoi@gmail.com>
2024-11-04 12:16:39 +0300

    added the header

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -36,9 +36,9 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                 
-    
+            
                 <li class="nav-item">
-                  <a href="" class="nav-link">
+                  <a href="{{route('posts.index')}}" class="nav-link">
                       <i class="nav-icon fas fa-tachometer-alt"></i>
                       <p>
                          Dashboard
