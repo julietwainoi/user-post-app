f303faf9b80d11cc0ae61ad0b2c18c5b7f07d07c
julietwainoi <juliewainoi@gmail.com>
2024-11-04 11:44:00 +0300

    added the header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Leave Management</title>
+    <title>user-post</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
