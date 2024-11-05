ef06ec06093a02b4e441b628481b4fa3730322d4
julietwainoi <juliewainoi@gmail.com>
2024-11-05 13:17:01 +0300

    added education personal details,work experience model

---
+++ database/migrations/2024_11_05_101412_create_github_repositories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('github_repositories', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('repo_name');
+            $table->string('repo_url');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('github_repositories');
+    }
+};
